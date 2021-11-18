1dc8a917984d8253be0ab8847dd3a74657890aac
Victor (Oi) Franco <victor.franco@zup.com.br>
2021-11-18 10:34:10 -0300

    feature: mn cli create project
    $ mn create-cli-app --build=gradle_kotlin --inplace --java-version=11 --test=junit --lang=kotlin mc-cli-example

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.5.21"
+    id("org.jetbrains.kotlin.kapt") version "1.5.21"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("io.micronaut.application") version "2.0.4"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.5.21"
+}
+
+version = "0.1"
+group = "mc.cli.example"
+
+val kotlinVersion=project.properties.get("kotlinVersion")
+repositories {
+    mavenCentral()
+}
+
+micronaut {
+    testRuntime("junit5")
+    processing {
+        incremental(true)
+        annotations("mc.cli.example.*")
+    }
+}
+
+dependencies {
+    kapt("info.picocli:picocli-codegen")
+    implementation("info.picocli:picocli")
+    implementation("io.micronaut:micronaut-runtime")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut.picocli:micronaut-picocli")
+    implementation("javax.annotation:javax.annotation-api")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    runtimeOnly("ch.qos.logback:logback-classic")
+    implementation("io.micronaut:micronaut-validation")
+
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    testImplementation("io.micronaut:micronaut-http-client")
+
+}
+
+
+application {
+    mainClass.set("mc.cli.example.McCliExampleCommand")
+}
+java {
+    sourceCompatibility = JavaVersion.toVersion("11")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
+    compileTestKotlin {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
+
+
+}
---
+++ src/main/kotlin/mc/cli/example/McCliExampleCommand.kt
@@ -0,0 +1,30 @@
+package mc.cli.example
+
+import io.micronaut.configuration.picocli.PicocliRunner
+import io.micronaut.context.ApplicationContext
+
+import picocli.CommandLine
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
+import picocli.CommandLine.Parameters
+
+@Command(name = "mc-cli-example", description = ["..."],
+        mixinStandardHelpOptions = true)
+class McCliExampleCommand : Runnable {
+
+    @Option(names = ["-v", "--verbose"], description = ["..."])
+    private var verbose : Boolean = false
+
+    override fun run() {
+        // business logic here
+        if (verbose) {
+            println("Hi!")
+        }
+    }
+
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            PicocliRunner.run(McCliExampleCommand::class.java, *args)
+        }
+    }
+}
